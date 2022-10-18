180d931f40cb93884ba8ffef7cde1c7c9208ec42
Mildivar <pac-man@yandex.ru>
2022-10-18 10:10:29 +1000

    dto/repository/viewmodel/activity packages added

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,8 +1,10 @@
-package ru.netology.nmedia
+package ru.netology.nmedia.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
