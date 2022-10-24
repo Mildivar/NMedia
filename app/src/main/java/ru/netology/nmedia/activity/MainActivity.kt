d24b21648586b6137ac46c7da6bbdded1bc83d9d
Mildivar <pac-man@yandex.ru>
2022-10-25 01:16:14 +1000

    06_recyclerview made

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -3,11 +3,9 @@ package ru.netology.nmedia.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
-import java.math.RoundingMode
-import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,58 +14,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        subscribe()
-        setupListeners()
-    }
 
-private fun subscribe() {
-    viewModel.data.observe(this) { post ->
-        binding.apply {
-            author.text = post.author
-            content.text = post.content
-            published.text = post.published
-            like.setImageResource(if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24)
-
-            likeCounter.text = countersCorrector(post.likesCounter)
-            shareCounter.text = countersCorrector(post.sharesCounter)
+        val adapter = PostAdapter(
+            {viewModel.likeById(it.id)},
+            {viewModel.shareById(it.id)}
+        )
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
         }
     }
-}
-    private fun setupListeners(){
-            binding.apply{
-                like.setOnClickListener {
-                    viewModel.like()
+
+
+//            binding.apply{
+//                like.setOnClickListener {
+//                    viewModel.like()
 //                post.likedByMe = !post.likedByMe
 //                like.setImageResource(
 //                    if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
 //                )
 //                if (post.likedByMe) post.likesCounter++ else post.likesCounter--
 //                likeCounter.text = countersCorrector(post.likesCounter)
-                }
-
-                shares.setOnClickListener {
-                    viewModel.share()
+//                }
+//
+//                shares.setOnClickListener {
+//                    viewModel.shareById()
 //                post.sharesCounter++
 //                shareCounter.text = countersCorrector(post.sharesCounter)
-                }
+//                }
             }
-        }
-        }
+//        }
 
 
-private fun countersCorrector(item: Int): String {
-    return when (item) {
-        in 1000..9999 -> "${roundOffDecimal(item / 1000.0)}K"
-        in 10_000..999_999 -> "${(item / 1000)}K"
-        in 1_000_000..1_000_000_000 -> "${roundOffDecimal(item / 1_000_000.0)}M"
-        else -> "$item"
-    }
-}
 
-private fun roundOffDecimal(number: Double): String {
-    val df = DecimalFormat("#.#")
-    df.roundingMode = RoundingMode.FLOOR
-    return df.format(number).toString()
-}
 
 
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -6,28 +6,54 @@ import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.viewmodel.PostRepository
 
 class PostRepositoryInMemoryImpl : PostRepository {
-    private var post = Post(
-        id = 1,
-        author = "Нетология. Университет интернет-профессий будущего",
-        content = " Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-        published = "21 мая в 18:36",
-        likedByMe = false,
-        likesCounter = 999999,
-        sharesCounter = 990
+    private var posts = listOf(
+        Post(
+            id = 3,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = " Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false,
+            likesCounter = 999999,
+            sharesCounter = 990
+        ),
+        Post(
+            id = 2,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = " Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false,
+            likesCounter = 999999,
+            sharesCounter = 990
+        ),
+        Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = " Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false,
+            likesCounter = 999999,
+            sharesCounter = 990
+        )
     )
 
-    private val data = MutableLiveData(post)
+    private val data = MutableLiveData(posts)
 
-    override fun get(): LiveData<Post> = data
+    override fun get(): LiveData<List<Post>> = data
 
-    override fun like() {
-        post = post.copy(likedByMe = !post.likedByMe)
-        if (post.likedByMe) post.likesCounter++ else post.likesCounter--
-        data.value = post
+    override fun likeById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                likedByMe = !it.likedByMe,
+                likesCounter = if (it.likedByMe) it.likesCounter - 1 else it.likesCounter + 1
+            )
+        }
+        data.value = posts
     }
 
-    override fun share() {
-        post.sharesCounter++
-        data.value = post
+    override fun shareById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(sharesCounter = it.sharesCounter+1)
+        }
+        data.value = posts
     }
 }
